58b8132b4f66e7de80eb4e513273d5a725e29736
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2022-03-10 08:58:15 +0300

    Initial Commit

---
+++ src/main/java/com/example/petprojectexample/PetProjectExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.petprojectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petprojectexample/PetProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petprojectexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
