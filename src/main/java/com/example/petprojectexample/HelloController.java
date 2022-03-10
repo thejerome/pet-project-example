d230ec86df7dffaad3721763ff29596e1253d6ad
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2022-03-10 09:25:54 +0300

    Hello Username feature + added test for Hello World

--- src/main/java/com/example/petprojectexample/HelloController.java
+++ src/main/java/com/example/petprojectexample/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.petprojectexample;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class HelloController {
     @GetMapping("hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping(path = "hello", params = "name")
+    public String helloUsername(@RequestParam("name") String name) {
+        return "Hello, " + name + "!";
     }
 
 }
